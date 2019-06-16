c5bd5f6bd3d0ffd72a88698ed7b32339104d4fad
Karazakoff <karazakuulu.54@gmail.com>
2019-06-16 16:03:18 +0600

    this is true solution for task6 in part3

--- tusk6.py
+++ tusk6.py
@@ -2,16 +2,26 @@ number = int(input())
 for c in range(number):
     str = input()
     mas = []
+    mas2 = []
     for i in range(len(str)):
         if str[i] == '(' or str[i] == '[' or str[i] == '{':
             mas.append(str[i])
-        if str[i] == ')' and mas[len(mas) - 1] == '(':
-            mas.pop()
-        if str[i] == ']' and mas[len(mas) - 1] == '[':
-            mas.pop()
-        if str[i] == '}' and mas[len(mas) - 1] == '{':
-            mas.pop()
-    if mas == []:
+        if str[i] == ')':
+            if mas[len(mas) - 1] == '(':
+                mas.pop()
+            else:
+                mas2.append(str[i])
+        if str[i] == ']':
+            if mas[len(mas) - 1] == '[':
+                mas.pop()
+            else:
+                mas2.append(str[i])
+        if str[i] == '}':
+            if mas[len(mas) - 1] == '{':
+                mas.pop()
+            else:
+                mas2.append(str[i])
+    if mas == [] and mas2 == []:
         print("YES")
     else:
         print("NO")
